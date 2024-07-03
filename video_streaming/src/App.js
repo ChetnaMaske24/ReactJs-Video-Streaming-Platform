4758c34386bce4b6f9550afd48c49002285a09e1
Chetna <cmaske580@gmail.com>
2024-07-03 14:53:22 +0530

    HeaderSection completed

--- video_streaming/src/App.js
+++ video_streaming/src/App.js
@@ -1,15 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
+import MobileNavigation from './components/MobileNavigation';
 
 
 function App() {
   return (
-   <main>
+   <main className='pb-14 lg:pb-0'>
        <Header />
        <div className='min-h-[90vh]'>
             <Outlet/>
         </div>
+        <MobileNavigation />
    </main>
   );
 }
