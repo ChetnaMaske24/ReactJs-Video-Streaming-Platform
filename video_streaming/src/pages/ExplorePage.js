297e862a2e487567347a1fc2bdd4eec96607a06c
Chetna <cmaske580@gmail.com>
2024-07-06 23:48:14 +0530

    start implementing ExplorePage

--- video_streaming/src/pages/ExplorePage.js
+++ video_streaming/src/pages/ExplorePage.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import Card from '../components/Card'
 
 const ExplorePage = () => {
+  
   return (
-    <div>
-      ExplorePage
+    <div className='py-16'>
+        <div className='container mx-auto'>
+            <h3 className='capitalize text-lg lg:text-xl font-semibold my-3'>Popular {params.explore} show</h3>
+
+            <div className='grid grid-cols-[repeat(auto-fit,230px)] gap-6 justify-center lg:justify-start'>
+              {
+                data.map((exploreData,index)=>{
+                  return(
+                    <Card data={exploreData} key={exploreData.id+"exploreSEction"} media_type={params.explore}/>
+                  )
+                })
+              }
+            </div>
+        </div>
     </div>
   )
 }
